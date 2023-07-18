d985b2640b2a4ddda33c2499fb7eb902d6588382
vkehfdl1 <jeffrey@edai-ad.com>
2023-07-18 09:52:41 +0900

    grammar prompt and modified 42 description

--- prompts.py
+++ prompts.py
@@ -90,7 +90,7 @@ def talk_prompt(paragraph, question, choices, question_plus="", no_paragraph=Fal
         "role": "system", "content": system_prompt
     }, {
         "role": "user", "content": user_prompt
-    }], temperature=0.0)
+    }], top_p=0)
     return completion.choices[0].message.content
 
 
@@ -143,5 +143,65 @@ def literature_prompt(paragraph, question, choices, question_plus="", no_paragra
         "role": "system", "content": system_prompt
     }, {
         "role": "user", "content": user_prompt
-    }], temperature=0.0)
+    }], top_p=0)
+    return completion.choices[0].message.content
+
+
+def grammar_prompt(paragraph, question, choices, question_plus="", get_prompt=False, no_paragraph=False):
+    system_prompt = """
+        당신은 국어 시험 문제를 푸는 대한민국의 고3 수험생으로서 최종 정답을 고르시오.
+
+        '지문 속 목적어의 성격'과 '선택지 속 목적어의 성격'이 서로 같은 선택지를 1개만 고르세요.
+        모두 같은 선택지는 무조건 1개만 존재합니다.
+
+        문제를 풀이할 때 5개의 모든 선택지를 검토하세요.
+
+        자료나 돈처럼 실제 손으로 만질 수 있는 것은 '실제적인 단어'입니다.
+        관심, 집중, 인기 이론처럼, 실제 손으로 만질 수 없는 것은 '추상적인 단어'입니다.
+
+        다음의 형식대로만 답변하세요.
+        최종 정답: (지문 속 목적어와 선택지 속 목적어의 성격이 서로 같은 선택지는 "(최종 정답)"입니다.
+        1번: - 지문 속 동사ⓐ의 목적어: "(목적어)" + 지문 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+             - 선택지 속 동사ⓐ의 목적어: "(목적어)" + 선택지 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+        2번: - 지문 속 동사ⓑ의 목적어: "(목적어)" + 지문 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+             - 선택지 속 동사ⓑ의 목적어: "(목적어)" + 선택지 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+        3번: - 지문 속 동사ⓒ의 목적어: "(목적어)" + 지문 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+             - 선택지 속 동사ⓒ의 목적어: "(목적어)" + 선택지 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+        4번: - 지문 속 동사ⓓ의 목적어: "(목적어)" + 지문 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+             - 선택지 속 동사ⓓ의 목적어: "(목적어)" + 선택지 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+        5번: - 지문 속 동사ⓔ의 목적어: "(목적어)" + 지문 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+             - 선택지 속 동사ⓔ의 목적어: "(목적어)" + 선택지 속 목적어의 성격 : "(실제적인 단어 or 추상적인 단어)"
+
+    """
+    if not no_paragraph:
+        user_prompt = f"""
+            지문 :
+            {paragraph}
+        """
+    else:
+        user_prompt = ""
+    if question_plus:
+        user_prompt += f"""
+            이 문제는 아래와 같이 <보기>가 주어져 있습니다. 문제의 각 선택지들을 해결하기 위한 배경 지식을 설명해 주고 있는 것이 <보기>로써, 각 선택지들을 지문과 연결시키고, <보기>의 지식을 활용하면 각 선택지의 참과 거짓을 판단할 수 있습니다.
+            문제를 해결할 때, 반드시 <보기>의 내용을 이용해서 문제를 해결해야 합니다.
+            <보기> :
+            {question_plus}
+        """
+    user_prompt += f"""
+        질문 :
+        {question}
+        선택지 :
+        1번 - {choices[0]}
+        2번 - {choices[1]}
+        3번 - {choices[2]}
+        4번 - {choices[3]}
+        5번 - {choices[4]}
+    """
+    if get_prompt:
+        return system_prompt +"\n\n" +user_prompt
+    completion = openai.ChatCompletion.create(model="gpt-4", messages=[{
+        "role": "system", "content": system_prompt
+    }, {
+        "role": "user", "content": user_prompt
+    }], top_p=0)
     return completion.choices[0].message.content
